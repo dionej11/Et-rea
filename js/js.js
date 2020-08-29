e88244c2acaeb161056c7c7063e00553b4db7b7b
Daniela <daniijuradob@gmail.com>
2020-08-28 22:03:49 -0500

    tiquete.html diseñado

--- js/js.js
+++ js/js.js
@@ -1,6 +1,12 @@
 const $menu = document.querySelector('#menu');
 const $nav = document.querySelector('#nav');
 const $volver = document.querySelector('#back');
+const $despegar = document.querySelector('#despegar');
+const $nombre= document.querySelector('#nombre');
+const $partida = document.querySelector('#desde');
+const $email = document.querySelector('#correo');
+const $form = document.querySelector('#contform');
+const $verif = document.querySelector('#verif');
 
 $menu.addEventListener('click',() =>{
     $nav.classList.remove("hide");
@@ -12,3 +18,31 @@ $volver.addEventListener('click', () =>{
     $volver.classList.add("hide");
     $nav.classList.add("hide");
 })
+$despegar.addEventListener('click',(e) =>{
+    var valor=validarForm($nombre.value, $partida.value, $email.value);
+    debugger
+    if(valor==1){
+        alert("Complete los datos");
+        e.preventDefault();
+    }else{        
+        $verif.classList.remove("hide");
+        $form.classList.add("hide");
+        e.preventDefault();
+    }
+})
+function validarForm(nombre,desde,correo){
+    if((nombre=="" || desde=="") || correo==""){
+        if(nombre==""){
+            document.querySelector('#nombre').focus();
+        }
+        if(desde==""){
+            document.querySelector('#desde').focus();
+        }
+        if(correo==""){
+            document.querySelector('#correo').focus();
+        }              
+        return 1;
+    }else{
+        return 0;
+    } 
+}
