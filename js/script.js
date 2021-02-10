cc1cc32303817fe1385fe1aed72cae2eb3bc7cf7
Daniela <daniijuradob@gmail.com>
2021-02-10 18:13:50 -0500

    galeria clickeable lista

--- js/script.js
+++ js/script.js
@@ -18,79 +18,39 @@ function mostrarScroll(){
 }
 window.addEventListener('scroll', mostrarScroll);
 
-var img;
-var modal;
-
-$('.galeria__img').click(function(e){
-    img = e.target.src;
-    modal= '<div class="modal" id="modal"><img src="'+img+'"class="modal__img"><div class="modal__boton" id="modal__boton"><img src="img/galery draws/close.png"></div><div class="modal__back" id="modal__back"><img src="img/galery draws/prev.png"></div><div class="modal__next" id="modal__next"><img src="img/galery draws/next.png"></div></div>';
-    $('.galeria_esp').append(modal);
-
-    $('#modal__boton').click(function(){
-        cerrar();
-    });
-    $('#modal__next').click(function(){
-        siguiente();
-    });
-    $('#modal__back').click(function(){
-        atras();
-    });
-});
+const modal_img = document.querySelector('.modal__img');
+const modal= document.querySelector('#modal');
+const imagenes_vec = document.querySelectorAll('.galeria__img');
+const cerrar = document.querySelector("#modal__boton");
+const volver = document.querySelector("#modal__back");
+const seguir = document.querySelector("#modal__next");
 
 
-$(document).keyup(function(e){
-    if (e.which == 27){
-        cerrar();
-    }
-    if (e.which == 39){
-        siguiente();
-    }
-    if (e.which == 37){
-        atras();
-    }
+for (let i = 0; i < imagenes_vec.length; i++) {
+    const element = imagenes_vec[i];
+    element.addEventListener('click', (e)=>{
+        modal.style.display="flex";
+        modal_img.setAttribute("src", e.target.src);
+    })
+}
+cerrar.addEventListener('click', (e)=>{
+    modal.style.display="none";
 })
-function siguiente(){
-    var part= img.split(".");
-    var numim= part[0].charAt(part[0].length - 1);
-    var num= parseInt(numim) + 1;
-    if(num >8){
-        num=1;
+seguir.addEventListener('click', (e)=>{
+    const ruta_actual = modal_img.src;
+    const vec = ruta_actual.split(".");
+    let num_img = parseInt(vec[vec.length -2][(vec[vec.length -2]).length -1]);
+    if (num_img == imagenes_vec.length) {
+        num_img=1;
     }
-    $('#modal').remove();
-    modal='<div class="modal" id="modal"><img src="img/galery draws/imagen'+num+'.png" class="modal__img"><div class="modal__boton" id="modal__boton"><img src="img/galery draws/close.png"></div><div class="modal__back" id="modal__back"><img src="img/galery draws/prev.png"></div><div class="modal__next" id="modal__next"><img src="img/galery draws/next.png"></div></div>';
-    $('.galeria_esp').append(modal);
-    img= "img/galery draws/imagen"+num+".png";
-    $('#modal__boton').click(function(){
-        cerrar();
-    });
-    $('#modal__next').click(function(){
-        siguiente();
-    });
-    $('#modal__back').click(function(){
-        atras();
-    });
-}
-function atras(){
-    var part= img.split(".");
-    var numim= part[0].charAt(part[0].length - 1);
-    var num= parseInt(numim) - 1;
-    if(num == 0){
-        num=8;
+    modal_img.setAttribute("src","img/galery draws/imagen"+(num_img + 1)+".png");
+})
+volver.addEventListener('click', (e)=>{
+    const ruta_actual = modal_img.src;
+    const vec = ruta_actual.split(".");
+    let num_img = parseInt(vec[vec.length -2][(vec[vec.length -2]).length -1]);
+    if (num_img == 1) {
+        num_img= imagenes_vec.length+1;
     }
-    $('#modal').remove();
-    modal='<div class="modal" id="modal"><img src="img/galery draws/imagen'+num+'.png" class="modal__img"><div class="modal__boton" id="modal__boton"><img src="img/galery draws/close.png"></div><div class="modal__back" id="modal__back"><img src="img/galery draws/prev.png"></div><div class="modal__next" id="modal__next"><img src="img/galery draws/next.png"></div></div>';
-    $('.galeria_esp').append(modal);
-    img= "img/galery draws/imagen"+num+".png";
-    $('#modal__boton').click(function(){
-        cerrar();
-    });
-    $('#modal__next').click(function(){
-        siguiente();
-    });
-    $('#modal__back').click(function(){
-        atras();
-    }); 
-}
-function cerrar(){
-    $('#modal').remove();
-}
+    modal_img.setAttribute("src","img/galery draws/imagen"+(num_img-1)+".png");
+})
