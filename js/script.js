f20cd543caab825e9f5ad44829a1710391ea4f1c
Daniela <daniijuradob@gmail.com>
2021-03-04 14:06:07 -0500

    archivos eliminados

--- js/script.js
+++ js/script.js
@@ -81,3 +81,15 @@ function atras(){
 function equis(){
     modal.style.display="none";
 }
+/**********************carousel*******************************/
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel__lista'), {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: '.carousel__indicadores',
+        arrows: {
+            prev: '.carousel__anterior',
+            next: '.carousel__siguiente'
+        }
+    });
+}); 
